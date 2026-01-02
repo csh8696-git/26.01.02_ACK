17e65bf096c4ccd46beef820ef9127ad248af548
csh8696-git <csh8696@gmail.com>
2026-01-02 10:06:56 +0900

    add_moreInfo
    타인풀이추가

--- solution2.cpp
+++ solution2.cpp
@@ -20,3 +20,19 @@ int solution(vector<int> absolutes, vector<bool> signs) {
     }
     return answer;
 }
+
+int solution(std::vector<int> absolutes, std::vector<bool> signs) {
+    int answer = 0;
+    for (int i = 0; i < signs.size(); i++)
+        answer += absolutes[i] * (signs[i] ? 1 : -1);
+    return answer;
+}
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for (int i = 0; i < signs.size(); i++) {
+        if (!signs[i]) absolutes[i] *= -1;
+        answer += absolutes[i];
+    }
+    return answer;
+}
