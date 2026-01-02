983a1bf2063c1deab552b8ec8bf0cc437553acdb
csh8696-git <csh8696@gmail.com>
2026-01-02 09:34:17 +0900

    solution2_26
    음양 더하기

---
+++ solution2.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    vector<int> num;
+    for (int i = 0; i < absolutes.size(); i++)
+    {
+        if (signs[i] == true)
+        {
+            num.push_back(absolutes[i]);
+        }
+        else
+        {
+            num.push_back(-absolutes[i]);
+        }
+        answer += num[i];
+    }
+    return answer;
+}
